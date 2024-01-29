ad001161de7f22255d2e74a2b2d92a46b0bd895d
Xavier-Hsiao <tim88776@gmail.com>
2024-01-29 12:17:28 +0800

    Build: add formatDate function to CityItem

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -1,5 +1,12 @@
 import styles from "./CityItem.module.scss";
 
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("tw", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
 export default function CityItem({ city }) {
   const { cityName, emoji, date } = city;
 
@@ -7,7 +14,7 @@ export default function CityItem({ city }) {
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{date}</time>
+      <time className={styles.date}>{formatDate(date)}</time>
     </li>
   );
 }
