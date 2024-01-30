bfeb341b73d3cb730f6928fefd4bd1a6542b4d5b
Xavier-Hsiao <tim88776@gmail.com>
2024-01-30 13:22:03 +0800

    Build: read and set longitude and latitude query params in Map

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -2,6 +2,5 @@ import { useParams } from "react-router";
 
 export default function City() {
   const { id } = useParams();
-  console.log(id);
-  return <div>City boy!</div>;
+  return <h1>City boy! {id}</h1>;
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,18 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.scss";
 
 export default function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   return (
     <div className={styles.mapContainer}>
-      
+      <h1>Latitude: {lat}</h1>
+      <h1>Longitude: {lng}</h1>
+
+      <button onClick={() => setSearchParams({ lat: 69, lng: 69 })}>
+        Generate Fake position
+      </button>
     </div>
   );
 }
