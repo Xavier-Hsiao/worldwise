6dcd9a73947684e87d367520b216aa5a7af70681
Xavier-Hsiao <tim88776@gmail.com>
2024-01-30 13:43:07 +0800

    Build: configure route to Form

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -4,6 +4,7 @@ import CityItem from "../CityItem/CityItem";
 import Message from "../Message/Message";
 
 export default function CityList({ cities, isLoading }) {
+
   if (isLoading) return <Spinner />;
 
   // To check if there is no city on list
@@ -12,12 +13,16 @@ export default function CityList({ cities, isLoading }) {
       <Message message={"Add your first city by clicking a city on the map"} />
     );
   }
-  
+
   return (
-    <ul className={styles.cityList}>
-      {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
-      ))}
-    </ul>
+    <>
+      <ul className={styles.cityList}>
+        {cities.map((city) => (
+          <CityItem city={city} key={city.id} />
+        ))}
+      </ul>
+
+      
+    </>
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,12 @@
 import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router";
 import styles from "./Map.module.scss";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const navigate = useNavigate();
   return (
     <div className={styles.mapContainer}>
       <h1>Latitude: {lat}</h1>
@@ -13,6 +15,7 @@ export default function Map() {
       <button onClick={() => setSearchParams({ lat: 69, lng: 69 })}>
         Generate Fake position
       </button>
+      <button onClick={() => navigate("form")}>Go to Form</button>
     </div>
   );
 }
