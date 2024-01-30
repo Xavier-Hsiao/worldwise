1185ac203a9104f829f30b208fcd093db941c157
Xavier-Hsiao <tim88776@gmail.com>
2024-01-30 13:36:12 +0800

    Build: create Button component template

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import styles from "./Button.module.scss";
+
+export default function Button({ children, type, onClick }) {
+  return (
+    <button className={`${styles.btn} ${styles[type]}`}>{children}</button>
+  );
+}
