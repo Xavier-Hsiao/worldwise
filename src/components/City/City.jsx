a30a1358526d2f4b8ba54f6b8142d9edbecc0060
Xavier-Hsiao <tim88776@gmail.com>
2024-02-01 22:24:26 +0800

    Build: add City view structure and basic styling

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import useCity from "../../hooks/useCityContext";
 import { useEffect } from "react";
 import styles from "./City.module.scss";
+import Button from "../Button/Button";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -22,9 +23,37 @@ export default function City() {
   }, [id]);
 
   return (
-    <div>
-      {emoji}
-      {cityName}
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        <Button type="back">Back</Button>
+      </div>
     </div>
   );
 }
